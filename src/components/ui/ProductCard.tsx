ea9ef67f479d3dbb186f98b04680cdddf3f741ec
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-05-18 22:05:18 +0600

    product card : add to cart , compare button added without functionality

--- src/components/ui/ProductCard.tsx
+++ src/components/ui/ProductCard.tsx
@@ -1,11 +1,13 @@
 import { TProductDetail } from "@/types/globalTypes";
 import { CiShop } from "react-icons/ci";
+import { IoMdCart, IoMdGitCompare } from "react-icons/io";
 import { Link } from "react-router-dom";
+import { Button } from "./button";
 
 const ProductCard = ({ product }: { product: TProductDetail }) => {
   // console.log(product?.discount);
   return (
-    <div className="ProductCardContainer bg-white border border-gray-300 shadow-md group  rounded-md overflow-auto hover:shadow-lg hover:scale-[1.01] duration-200 ">
+    <div className="ProductCardContainer bg-white border border-gray-300 shadow-md   rounded-md overflow-auto hover:shadow-lg hover:scale-[1.01] duration-200 ">
       <Link to={`/product/detail/${product?.id}`}>
         <div className="ProductCardWrapper flex flex-col justify-between gap-y-1  ">
           {/* product image section  */}
@@ -22,9 +24,9 @@ const ProductCard = ({ product }: { product: TProductDetail }) => {
             </div>
           </div>
 
-          <div className="prodDes mb-2 p-3 group-hover:text-prime100  ">
+          <div className="prodDes mb-2 p-3 hover:text-prime200  ">
             {/* prod name  */}
-            <h1 className=" font-medium mb-2 text-lg "> {product?.name} </h1>
+            <h1 className=" font-medium mb-2  "> {product?.name} </h1>
 
             <div className="productPriceShopName flex justify-between items-center ">
               {/* prod price  */}
@@ -54,6 +56,21 @@ const ProductCard = ({ product }: { product: TProductDetail }) => {
           </div>
         </div>
       </Link>
+
+      {/*  */}
+      <div className="buttonSectio pb-2 px-2 flex justify-between   ">
+        <Button className=" w-[75%]  text-center text-sm font-semibold text-white transition duration-100  bg-prime100 hover:bg-prime200 ">
+          <span>
+            <IoMdCart />
+          </span>
+          <span> Add To Cart</span>
+        </Button>
+
+        <Button className=" bg-prime100 hover:bg-prime200">
+          <IoMdGitCompare />
+        </Button>
+      </div>
+      {/*  */}
     </div>
   );
 };
