231d683ae91a0af2ce50c8fe59c2c9f2d2add6e4
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-08-03 14:23:05 +0600

    navigation bottom section added

--- src/components/ui/Navbar/NavbatBottom.tsx
+++ src/components/ui/Navbar/NavbatBottom.tsx
@@ -1,6 +1,24 @@
-import { useState } from "react";
+import { useGetAllCategoryQuery } from "@/redux/features/category/category.api";
+import { useEffect, useState } from "react";
 import { RiCloseFill, RiMenu3Fill } from "react-icons/ri";
 import { Link } from "react-router-dom";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "../navigation-menu";
+
+type TCategoryOption = {
+  name: string;
+  value: string;
+};
+
+type TCategory = {
+  id: string;
+  name: string;
+};
 
 const Links = [
   { name: "Home", link: "/" },
@@ -11,17 +29,71 @@ const Links = [
 
 const NavbatBottom = () => {
   const [open, setOpen] = useState(false);
+  const [categoryOptions, setCategoryOptions] = useState<TCategoryOption[]>([]);
+
+  const { data: categoryData, isLoading: categoryDataLoading } =
+    useGetAllCategoryQuery(undefined);
+
+  // console.log(categoryData?.data )
+
+  //   ! effect for get category data
+  useEffect(() => {
+    if (categoryData?.data) {
+      const modifiedData = categoryData?.data?.map((item: TCategory) => {
+        const optionValue = {
+          name: item?.name,
+          value: item?.id,
+        };
+
+        return optionValue;
+      });
+
+      const initialData: TCategoryOption = {
+        name: "All",
+        value: "",
+      };
+
+      setCategoryOptions([initialData, ...modifiedData]);
+    }
+  }, [categoryData?.data, categoryDataLoading]);
+
+  // console.log(categoryOptions);
 
   return (
-    <div className="NavbatBottomContainer flex justify-between py-2 ">
+    <div className="NavbatBottomContainer flex justify-between items-center py-2 ">
       {/* left category section  */}
 
-      <div className="leftCategorySection bg-violet-400 ">
-        <h1>left category section </h1>
+      <div className="leftCategorySection  ">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger className=" border border-prime100  ">
+                All Categories
+              </NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className="text-sm font-medium text-gray-800 w-[11rem] xsm:w-[16rem] ">
+                  {categoryOptions &&
+                    categoryOptions?.map(
+                      (item: { name: string; value: string }, ind: number) => (
+                        <li
+                          key={ind}
+                          className="w-full border-b border-gray-300"
+                        >
+                          <Link to={`/products?ParamCategory=${item?.value}`}>
+                            <p className="  ml-6 py-2  ">{item?.name}</p>
+                          </Link>
+                        </li>
+                      )
+                    )}
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
       </div>
 
       {/* right menu section  */}
-      <div className="rightMenuSection bg-teal-300 ">
+      <div className="rightMenuSection  ">
         {/* Menu icon */}
         <div
           onClick={() => setOpen(!open)}
@@ -33,15 +105,15 @@ const NavbatBottom = () => {
         {/* rigth section  */}
         {/* linke items */}
         <ul
-          className={`absolute bg-white shadow-md 
-            md:shadow-none z-[-1] left-0 w-full pl-10 
-            md:flex md:items-center pb-8 md:pb-0 
+          className={`absolute bg-gray-100 shadow-md 
+            md:shadow-none z-[-1] left-0 w-full pr-5 
+            md:flex text-end md:items-center pb-8 md:pb-0 
             md:static md:bg-transparent md:z-auto 
             md:w-auto md:pl-0 transition-all 
             duration-300 ease-in 
             text-xs xsm:text-sm sm:text-base 
             md:text-xs xmd:text-sm xlm:text-base 
-            ${open ? "top-[3.2rem] block" : "top-[-490px]"}`}
+            ${open ? "top-[6.8rem] block" : "top-[-490px]"}`}
           style={{
             backdropFilter: "blur(3rem)",
           }}
@@ -50,7 +122,7 @@ const NavbatBottom = () => {
             Links.map((link, index) => (
               <li
                 key={index}
-                className="  my-5 xsm:my-7 ml-4 xl:ml-6 md:my-0  font-semibold uppercase"
+                className="   my-5 xsm:my-7 ml-4 xl:ml-6 md:my-0  font-semibold uppercase"
               >
                 <Link
                   to={link.link}
